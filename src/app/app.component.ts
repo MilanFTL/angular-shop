1ec2838d363eb93aaedc2f310d7178e9cf5da5c3
MilanFTL <milan@berthold-records.de>
2023-08-17 15:53:20 +0200

    shop-formatting

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ export interface Product {
   description: string;
   id: number;
   inCart: boolean;
+  img: string;
 }
 
 @Component({
@@ -25,11 +26,46 @@ export class AppComponent {
 
   ngOnInit(): void {
     this.stock = [
-      { name: 'car', description: 'a simple car', id: 1, inCart: true },
-      { name: 'fasd', description: 'a simple car', id: 1, inCart: true },
-      { name: 'cdddar', description: 'a simple car', id: 1, inCart: true },
-      { name: 'ghg', description: 'a simple car', id: 1, inCart: true },
-      { name: 'table', description: 'a big table', id: 2, inCart: false },
+      {
+        name: 'car',
+        description:
+          'Ein Elfer in seiner reinsten Form. Entwickelt in Flacht. Und gebaut für einen einzigen Zweck: maximalen Fahrspaß. ',
+        id: 1,
+        inCart: true,
+        img: 'assets/porsche-1851246_1280.jpg',
+      },
+      {
+        name: 'fasd',
+        description:
+          'Ein Elfer in seiner reinsten Form. Entwickelt in Flacht. Und gebaut für einen einzigen Zweck: maximalen Fahrspaß. ',
+        id: 1,
+        inCart: true,
+        img: 'assets/table.jpg',
+      },
+      {
+        name: 'cdddar',
+        description:
+          'Ein Elfer in seiner reinsten Form. Entwickelt in Flacht. Und gebaut für einen einzigen Zweck: maximalen Fahrspaß. ',
+        id: 1,
+        inCart: true,
+        img: 'assets/porsche-1851246_1280.jpg',
+      },
+      {
+        name: 'ghg',
+        description:
+          'Ein Elfer in seiner reinsten Form. Entwickelt in Flacht. Und gebaut für einen einzigen Zweck: maximalen Fahrspaß. Der 911 S/T vereint mit seinem puristischen Leichtbaudesign, seiner atemberaubenden GT High-Performance und der deutlich reduzierten Dämmung die Essenz aus 60 Jahren 911. Das Ergebnis: eine bisher unerreichte Synthese aus Purismus, Agilität und einzigartiger Fahrdynamik.',
+        id: 1,
+        inCart: true,
+        img: 'assets/porsche-1851246_1280.jpg',
+      },
+      {
+        name: 'table',
+        description:
+          'Ein Elfer in seiner reinsten Form. Entwickelt in Flacht. Und gebaut für einen einzigen Zweck: maximalen Fahrspaß. ',
+        id: 2,
+        inCart: false,
+        img: 'assets/porsche-1851246_1280.jpg',
+      },
       // Add more objects as needed
     ];
   }
